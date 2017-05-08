ebe4b9d4608e2a04cf9a2fbf7385fb7c28b9f7b0
joannepeach <jlee20@cca.edu>
2017-05-07 17:13:56 -0700

    Create arrays-3.js

---
+++ arrays-3.js
@@ -0,0 +1,21 @@
+var shade = [10,20,45,25,60]; 
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  colorMode(HSB);
+  noStroke();
+		function mouseClicked() {
+append(shade,random(10,50));
+}
+  
+ for (var i = 0; i < shade.length; i += 1) {
+	fill(shade[i], 100, 100);
+  rect(i*width/shade.length, 0, width/shade.length, height);
+	}
+	}
+	function mouseClicked() {
+append(shade,random(10,50));
+}
